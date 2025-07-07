81a9e48e852c0324040cb4c191ef4baba444cab2
yashwanthB0312 <yashwanth.b2022cce@sece.ac.in>
2025-07-07 23:16:35 +0530

    completed

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://budgetiq-bwcd.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
